02c50da5e0dd522ff63705fbe3b56381f2706dfc
sellmyclicks <sellmyclicks@gmail.com>
2018-08-23 04:56:02 -0700

    edited login

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); 
 $con=mysqli_connect("localhost","college","collegedb","college");
 
 $uname=$_POST['uname'];
@@ -6,8 +7,13 @@ $pass=$_POST['pass'];
 
 $qa="select * from login where uname='$uname' and pass='$pass'";
 $res=mysqli_query($con,$qa);
+
 if($row=mysqli_fetch_array($res))
+
 {
+  // store session data
+  $_SESSION["uname"] = "$uname";
+  $_SESSION["pass"] = "$pass";
   if(($uname==$row['uname'])&&($pass==$row['pass']))
   {
     if($row['utype']=="student")
